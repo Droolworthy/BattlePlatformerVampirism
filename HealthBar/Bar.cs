f1f43150ab1bd5ead0b2cd9c0acee4211a617941
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2024-02-07 18:06:30 +0300

    Update Bar.cs

--- HealthBar/Bar.cs
+++ HealthBar/Bar.cs
@@ -1,12 +1,42 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public abstract class Bar : MonoBehaviour
 {
     [SerializeField] protected Slider Wellness;
+    [SerializeField] float _speed;
 
-    public void OnValueChanged(int value, int maxValue)
+    private Coroutine _coroutine;
+
+    public void ChangeValue(int value, int maxHealth)
     {
-        Wellness.value = (float)value / maxValue;
+        float targetValue = Wellness.value;
+
+        targetValue = (float)value / maxHealth;
+
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(TransformWellnessLevel(targetValue));
+    }
+
+    private IEnumerator TransformWellnessLevel(float targetValue)
+    {
+        bool isWork = true;
+
+        while (isWork)
+        {
+            Wellness.value = Mathf.MoveTowards(Wellness.value, targetValue, _speed * Time.deltaTime);
+
+            if (Wellness.value == targetValue)
+            {
+                isWork = false;
+            }
+
+            yield return null;
+        }
+
+        StopCoroutine(_coroutine);
     }
 }
